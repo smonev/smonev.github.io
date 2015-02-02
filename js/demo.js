86b247e8f6b55a3aa571d41d1563b155380000cd
Stoicho Monev <smonev@gmail.com>
2015-02-02 22:59:40 +0200

    demo updated

--- js/demo.js
+++ js/demo.js
@@ -1,35 +1,29 @@
 /** @jsx React.DOM */
 
-var PageView = React.createClass({displayName: 'PageView',
-  getInitialState: function() {
-    return {
-      startLoader: false
-    };
-  },
-
+var PageView = React.createClass({
   startLoader: function() {
-      this.setState({startLoader: true}); 
+      this.refs.pageLoader.animate();
   },
 
   render: function () {
     return (
-      React.DOM.div(null, 
-          PageLoader({show: this.state.startLoader}), 
-          React.DOM.input({type: "button", onClick: this.startLoader, value: "Fire Loader"})
-      )
+      <div>
+            <PageLoader ref="pageLoader" />
+            <input type="button" onClick={this.startLoader} value="Fire Loader"></input>
+      </div>
     );
   }
 });
 
-var PageLoader = React.createClass({displayName: 'PageLoader',
+var PageLoader = React.createClass({
   getInitialState: function() {
     var innerWidth = window.innerWidth;
+
     return {
-      loaderWidth: innerWidth,
-      loaderLeft: - innerWidth - 20,
-      inAnimation: false,
-      animationId: 0, 
-      pos: 0
+        loaderWidth: innerWidth,
+        loaderLeft: - innerWidth - 20,
+        animationId: 0,
+        pos: 0
     };
   },
 
@@ -37,9 +31,6 @@ var PageLoader = React.createClass({displayName: 'PageLoader',
       var that = this, animationTimer;
       animationTimer = setTimeout(function() {
           var  newLeft;
-          if (!that.state.inAnimation)  {
-            return;
-          }
 
           if (that.state.loaderLeft < 0){
               that.state.animationId = window.requestAnimationFrame(that.animate);
@@ -63,15 +54,10 @@ var PageLoader = React.createClass({displayName: 'PageLoader',
   },
 
   render: function () {
-      if (this.props.show) {
-        if (!this.state.inAnimation) {
-            this.state.inAnimation = true;
-            this.animate();
-        }
-      }
+
       var style = {
           width: this.state.loaderWidth,
-          height: '2px',
+          height: '3px',
           backgroundColor: '#c0392b',
           display: 'block',
           position: 'fixed',
@@ -83,9 +69,9 @@ var PageLoader = React.createClass({displayName: 'PageLoader',
       };
 
       return (
-          React.DOM.span({style: style}, '')
+          <span style={style}>{''}</span>
       );
   }
 });
 
-React.renderComponent(PageView(null), document.body);
+React.render(<PageView />, document.body);
